da788b9c2c4be9c74c9407be4f2864adee0204e9
ashrafmakandar <37826738+ashrafmakandar@users.noreply.github.com>
2025-07-09 14:50:12 +0530

    made api changes

--- api/headlines.js
+++ api/headlines.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const { country = 'in' } = req.query;
+  const { q = 'India' } = req.query;
 
   try {
-    const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`;
+    const url = `https://newsapi.org/v2/everything?q=${q}&apiKey=${process.env.NEWS_API_KEY}`;
     const response = await axios.get(url);
 
     const articles = response.data.articles.map((article) => ({
       title: article.title,
+      description:article.description,
+      urlToImage:article.urlToImage,
       source: article.source.name,
-      url: article.url
+      publishedAt:article.publishedAt,
+      url: article.url,
+      content:article.content
     }));
 
     res.status(200).json({ country, total: articles.length, articles });
